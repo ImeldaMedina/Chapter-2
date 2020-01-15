7be64f156caa0de95216984d83d98f87db1b7c19
Imelda Medina <imedina-castaneda@mail.greenriver.edu>
2020-01-15 15:22:12 -0500

    add js and html file

---
+++ chapter-2.js
@@ -0,0 +1,19 @@
+/*
+Imelda Medina
+1/15/20
+chapter-2
+ */
+for(let i=1; i<=100; i++){
+    if(i % 3 == 0 && i % 5 ==0){
+        console.log("Hee Haw!");
+    }
+    else if(i % 3 ==0){
+        console.log("Hee!");
+    }
+    else if(i % 5 == 0){
+        console.log("Haw!");
+    }
+    else{
+        console.log(i);
+    }
+}
